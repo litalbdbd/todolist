3fc2021271c4d98d7c3563fea681bd7b2b1cabaf
litalz <litalbdbd@gmail.com>
2020-05-18 19:13:01 +0300

    changed input and buttons css

--- todo.js
+++ todo.js
@@ -5,15 +5,15 @@ function add() {
     task.innerHTML = document.getElementById("myText").value;
     var x = document.createElement("img");
     x.setAttribute("src", "x.png");
+    x.setAttribute("class", "x");
+
     var sp1 = document.createElement("span");
     var sp2 = document.createElement("span");
     sp1.appendChild(x);
     sp2.appendChild(task)
-
     var task_x_container = document.createElement("DIV");
     task_x_container.appendChild(sp1);
     task_x_container.appendChild(sp2);
-
     document.getElementById("list").appendChild(task_x_container);
 
 
