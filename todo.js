b3b5d2b824a75759814e951d8597f6ed12783823
litalz <litalbdbd@gmail.com>
2020-05-18 18:46:37 +0300

    added x to every task

--- todo.js
+++ todo.js
@@ -1,9 +1,22 @@
 function add() {
 
-    var task = document.createElement("DIV");
-    task.setAttribute("id", "task");
+    var task = document.createElement("span");
+    task.setAttribute("class", "task");
     task.innerHTML = document.getElementById("myText").value;
-    document.getElementById("list").appendChild(task);
+    var x = document.createElement("img");
+    x.setAttribute("src", "x.png");
+    var sp1 = document.createElement("span");
+    var sp2 = document.createElement("span");
+    sp1.appendChild(x);
+    sp2.appendChild(task)
+
+    var task_x_container = document.createElement("DIV");
+    task_x_container.appendChild(sp1);
+    task_x_container.appendChild(sp2);
+
+    document.getElementById("list").appendChild(task_x_container);
+
+
 }
 
 function deleteAll() {
