47a30d47062a135ab8990e34a8fe076427d87682
litalz <litalbdbd@gmail.com>
2020-05-20 10:56:02 +0300

    added delete option to every task

--- todo.js
+++ todo.js
@@ -7,6 +7,7 @@ function add() {
     x.setAttribute("src", "x.png");
     x.setAttribute("class", "x");
 
+
     var sp1 = document.createElement("span");
     var sp2 = document.createElement("span");
     sp1.appendChild(x);
@@ -15,12 +16,23 @@ function add() {
     task_x_container.setAttribute("id", "task_x_container")
     task_x_container.appendChild(sp1);
     task_x_container.appendChild(sp2);
-    if (document.getElementById("myText").value != "")
+    if (document.getElementById("myText").value != "") {
         document.getElementById("list").appendChild(task_x_container);
-
+    }
 
 }
 
 function deleteAll() {
     document.querySelector("#list").innerHTML = "";
+}
+
+
+document.addEventListener("click", removeTask);
+
+function removeTask(e) {
+    var element = e.target;
+    if (element.className == 'x') {
+        console.log("vdvdsved");
+        e.target.parentElement.parentElement.remove();
+    }
 }
